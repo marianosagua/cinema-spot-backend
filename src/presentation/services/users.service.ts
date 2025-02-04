9a28d60dcdf5670af364222e508f01d535f7f55f
Mariano Sagua <marianosagua4343@gmail.com>
2025-02-04 18:13:08 -0300

    Refactoricé los métodos de recuperación de usuarios para incluir información sobre roles y gestioné errores en caso de que falten usuarios.

--- src/presentation/services/users.service.ts
+++ src/presentation/services/users.service.ts
@@ -3,25 +3,84 @@ import { User } from "../../interfaces/user";
 
 export class UsersService {
   async getUsers() {
-    const users = prismaClient.users.findMany();
-    return users;
+    const users = await prismaClient.users.findMany({
+      select: {
+        id: true,
+        first_name: true,
+        last_name: true,
+        email: true,
+        roles: {
+          select: {
+            name: true,
+          },
+        },
+        created_at: true,
+        updated_at: true,
+        email_validated: true,
+      },
+    });
+
+    if (!users) {
+      throw new Error("Users not found");
+    }
+
+    const transformedUsers = users.map((user) => ({
+      id: user.id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      role: user.roles.name,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+      email_validated: user.email_validated,
+    }));
+
+    return transformedUsers;
   }
 
   async getUserById(id: string) {
-    const user = prismaClient.users.findUnique({
+    const user = await prismaClient.users.findFirst({
       where: {
         id,
       },
+      select: {
+        id: true,
+        first_name: true,
+        last_name: true,
+        email: true,
+        roles: {
+          select: {
+            name: true,
+          },
+        },
+        created_at: true,
+        updated_at: true,
+        email_validated: true,
+      },
     });
-    return user;
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    return {
+      id: user.id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      role: user.roles.name,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+      email_validated: user.email_validated,
+    };
   }
 
   async createUser(userData: User) {
     const { role, ...dataWithoutRole } = userData;
-    const newUser = prismaClient.users.create({
+    const createdUser = prismaClient.users.create({
       data: dataWithoutRole,
     });
-    return newUser;
+    return createdUser;
   }
 
   async updateUser(id: string, userData: User) {
